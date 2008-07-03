e83c9a1a99ed598d9436d5584750b022bd7a4849
GamerZ <GamerZ@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-07-03 09:53:24 +0000

    Remove nested dirname() call
    git-svn-id: https://plugins.svn.wordpress.org/wp-email/trunk@52945 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-email.php
+++ wp-email.php
@@ -34,7 +34,7 @@ define('EMAIL_SHOW_REMARKS', true);
 
 ### Load WP-Config File If This File Is Called Directly
 if (!function_exists('add_action')) {
-	$wp_root = dirname(dirname(dirname(dirname(__FILE__))));
+	$wp_root = '../../..';
 	if (file_exists($wp_root.'/wp-load.php')) {
 		require_once($wp_root.'/wp-load.php');
 	} else {
